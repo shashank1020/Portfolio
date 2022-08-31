cff19fe104cc8db70d9c58ce20d1901a0d8520d8
Shashank <shashankmaurya1020@gmail.com>
2022-08-31 15:03:57 +0530

    refactored code

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,11 @@
 // user data
 import {SiHtml5, SiStyledcomponents, SiNestjs, SiMaterialui} from "react-icons/si";
 import {DiCss3, DiReact} from "react-icons/di";
+import {IoLogoJavascript} from "react-icons/io";
 import bikeRental from './Assets/images/bike-rental.gif'
 import quizApp from './Assets/images/quiz-app.gif'
+import NFT from './Assets/images/nft.gif'
+
 export const USER = {
     fullName: 'Shashank Maurya',
     resumeLink: 'https://drive.google.com/file/d/1lWliF3wrNeIxVdpZftWSw5zSz0W7CR1F/view?usp=sharing',
@@ -12,16 +15,24 @@ export const USER = {
     email: 'shashankmaurya1020@gmail.com',
     projects: [
         {
+            title: 'NFT Market',
+            description: 'This is an android/IOS app build using React Native using expo, Buy, store, collect NFTS, exchange & earn crypto.',
+            technologies: [DiReact,IoLogoJavascript],
+            hostedLink: 'https://nefpro.netlify.app/',
+            github: 'https://github.com/shashank1020/NFTShowcase',
+            img: NFT
+        },
+        {
             title: 'Bike Rental',
             description: 'This is bike booking app, on any location in india, you can book any bike of like, from performance bike to mileage bikes, never stop traveling',
-            technologies: [DiReact, SiHtml5, DiCss3, SiStyledcomponents, SiNestjs, SiMaterialui],
+            technologies: [DiReact, SiHtml5, DiCss3, SiStyledcomponents, SiNestjs, SiMaterialui, IoLogoJavascript],
             github: 'https://github.com/shashank1020/BikeRental',
             img: bikeRental
         },
         {
             title: 'Quiz Website',
             description: 'This is quiz website, anyone can play any available quiz create by any user, only authenticated user can create new quiz',
-            technologies: [DiReact, SiHtml5, DiCss3, SiStyledcomponents, SiNestjs, SiMaterialui],
+            technologies: [DiReact, SiHtml5, DiCss3, SiStyledcomponents, SiNestjs, SiMaterialui, IoLogoJavascript],
             github: 'https://github.com/shashank1020/BikeRental',
             img: quizApp
         },
--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,52 +1,19 @@
 import React from "react";
 import "../../Assets/css/Project.css";
-import {SiChakraui, SiExpress, SiHtml5} from "react-icons/si";
-import {DiCss3, DiMongodb, DiReact, DiRequirejs} from "react-icons/di";
-import {IoLogoJavascript} from "react-icons/io";
-import ubuy from '../../Assets/images/ubuy.gif'
-import giftcard from '../../Assets/images/giftcard.gif'
-import youtube from '../../Assets/images/youtube.gif'
-import soundCloud from '../../Assets/images/soundCloud.gif'
+import {USER} from "../../Constants";
 
-const projectConstants = [
-    {
-        title: 'soundCloud Clone',
-        description: 'This is a online audio streaming and distribution platform that allows users to stream, and share music and podcasts.',
-        sub: 'This was a group project and contributors are Jevan, chandan, saurabh',
-        technologies: [SiChakraui, DiReact, DiCss3, DiMongodb, SiExpress],
-        hostedLink: 'https://web-jinke-music.vercel.app/',
-        github: 'https://github.com/BhaveshSuthar00/soundCld-client-side',
-        img: soundCloud,
-    },
-    {
-        title: 'Gift-card Clone',
-        description: 'Gift-card is a e-commerce website, It is an online gift-card retailer based in Pittsburgh, Pennsylvania. It is considered the largest online gift card retailer and has appeared on the Inc.',
-        sub: 'This was a group project and contributors are Yash, Kartik, Sumit, Mayar and Ankit',
-        technologies: [SiHtml5, DiCss3, DiRequirejs, IoLogoJavascript, DiMongodb, SiExpress],
-        hostedLink: 'https://enigmatic-beach-48011.herokuapp.com/',
-        github: 'https://github.com/BhaveshSuthar00/GiftCart-clone',
-        img: giftcard
-    },
-    {
-        title: 'Ubuy Clone',
-        description: 'Ubuy is not your usual e-commerce website, we provide our customers with access to an excellent variety of products. Today\'s shoppers want more in a shorter time period, and let\'s not forget the easy purchase methods and delivery to make it worth their while.',
-        sub: 'This was a group project and contributors are Suresh, Prachi and Ankit',
-        technologies: [SiHtml5, DiCss3, IoLogoJavascript],
-        hostedLink: 'https://ubuy-project.vercel.app/',
-        github: 'https://github.com/BhaveshSuthar00/ubuy-project',
-        img: ubuy
-    },
-    {
-        title: 'Youtube Clone',
-        description: '',
-        sub: 'Youtube clone is my individual project. I created this when I was learning REST API.',
-        technologies: [SiHtml5, DiCss3, IoLogoJavascript],
-        hostedLink: 'https://tube-ten.vercel.app/',
-        github: 'https://github.com/BhaveshSuthar00/tube',
-        img: youtube
-    }
-
-]
+export const Projects = () => {
+    return (
+        <>
+            <div className="section" id="project">
+                <h2 className="section__title different" data-aos="fade-right">
+                    <strong>Projects</strong>
+                </h2>
+                {USER.projects?.map((item) => <ProjectShowcase {...item} />)}
+            </div>
+        </>
+    );
+};
 
 const ProjectShowcase = (props) => {
     return(
@@ -95,16 +62,3 @@ const ProjectShowcase = (props) => {
         </div>
     )
 }
-
-export const Projects = () => {
-    return (
-        <>
-            <div className="section" id="project">
-                <h2 className="section__title different" data-aos="fade-right">
-                    <strong>Projects</strong>
-                </h2>
-                {projectConstants?.map((item) => <ProjectShowcase {...item} />)}
-            </div>
-        </>
-    );
-};
